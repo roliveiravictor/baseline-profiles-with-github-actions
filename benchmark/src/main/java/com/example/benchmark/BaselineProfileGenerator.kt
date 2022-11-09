469089b7937b12504619c7db0fae11b9a4e88b50
Rocha <victor.rocha@mercadolivre.com>
2022-11-08 23:02:08 -0300

    Add Baseline Profile generator

---
+++ benchmark/src/main/java/com/example/benchmark/BaselineProfileGenerator.kt
@@ -0,0 +1,24 @@
+package com.example.benchmark
+
+import androidx.benchmark.macro.ExperimentalBaselineProfilesApi
+import androidx.benchmark.macro.junit4.BaselineProfileRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@OptIn(ExperimentalBaselineProfilesApi::class)
+class BaselineProfileGenerator {
+
+    @get:Rule
+    val baselineProfileRule = BaselineProfileRule()
+
+    @Test
+    fun startup() =
+        baselineProfileRule.collectBaselineProfile(
+            packageName = "com.example.myapplication",
+            profileBlock = {
+                startActivityAndWait()
+            }
+        )
+}
